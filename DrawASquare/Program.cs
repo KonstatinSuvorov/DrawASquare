3103f22b8000fd376c12d2a03c8d5e02beb4e08c
Константин Суворов <kniaze84@outlook.com>
2022-11-09 10:49:39 +0300

    Добавьте файлы проекта.

---
+++ DrawASquare/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawASquare
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите символ: ");
+            char s = Convert.ToChar(Console.ReadLine());
+            Console.Write("Введите количество символов: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(s + " ");
+                }
+            }
+            Console.ReadLine();
+        }       
+    }
+}
